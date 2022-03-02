02881e3b06152392db82bd5a03184981f955f9f1
Pavel Yadlouski <pyadlous@redhat.com>
2022-03-02 16:36:27 +0100

    Rules simplifying algo 4.5

--- src/Minimaze.hs
+++ src/Minimaze.hs
@@ -4,34 +4,47 @@
 -- Rok: 2021/2022
 
 module Minimaze where
-import Types (Grammar (Grammar), Rule (left, right), NonTerminal, Rules, isOneNonTerm)
+import Types (Grammar (Grammar), Rule (left, right, Rule), NonTerminal, Rules, isOneNonTerm)
+-- import Debug.Trace (traceShow)
+
+
+uniq :: Eq a => [a] -> [a]
+uniq [] = []
+uniq (x:xs) = x : uniq (filter (/=x) xs)
 
 
 removeSimpleRules :: Grammar -> Grammar
-removeSimpleRules (Grammar nt t s rls) = Grammar nt t s rlsNew
+removeSimpleRules (Grammar nt t s rls) = Grammar nt t s newRls
     where
-        fnc = updateIfSimple rls
-        rlsNew = concatMap fnc nt
+        nA' = map (\x -> (x, findNA rls [x] x)) nt
+        newRls = concatMap (updateRules rls) nA'
 
-
-updateIfSimple :: Rules -> NonTerminal -> Rules
-updateIfSimple rls nt
-    | null smplRls = notSmplRls
-    | otherwise = notSmplRls ++ [] -- map  expandSimple nt rnt rls
+findNA :: Rules -> [NonTerminal] -> NonTerminal -> [NonTerminal]
+findNA rls previousNA nt
+    | previousNA == currNA = currNA
+    | otherwise = findNA rls currNA nt
     where
-        smplRls = findSimpleRules rls nt
-        notSmplRls = findNotSimpleRules rls nt
+        currNA = uniq (previousNA ++ concatMap (findSimpleRules rls) previousNA)
 
--- expandSimple :: NonTerminal -> NonTerminal -> Rules -> Rules
 
--- extractRightSideOfSimpleRule :: Rules -> [NonTerminal]
--- extractRightSideOfSimpleRule
+updateRules :: Rules -> (NonTerminal, [NonTerminal]) -> Rules
+updateRules rls (nt, nts) = newRls
+    where
+        nonSimpleA = concatMap (findNotSimpleRules rls) nts
+        newRls = [Rule {left=nt, right=x} | x <- map right nonSimpleA]
 
-findSimpleRules :: Rules -> NonTerminal -> Rules
-findSimpleRules rls nt = filter (\r -> (left r == nt) && isOneNonTerm (right r)) rls
 
 findNotSimpleRules :: Rules -> NonTerminal -> Rules
 findNotSimpleRules rls nt = filter (\r -> (left r == nt) && not (isOneNonTerm (right r))) rls
+
+
+findSimpleRules :: Rules -> NonTerminal -> [NonTerminal]
+findSimpleRules rls nt = [head $ right x | x <- simpleRls]
+    where
+        simpleRls = filter (\r -> (left r == nt) && isOneNonTerm (right r)) rls
+
+
+
 
 -- fnc :: NonTerminal -> Rules -> Rules
 -- fnc nt = filter (\r -> left r == nt)
